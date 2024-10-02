f5bd7d30835276d36fb28ca90456cdc6bbd1373a
Wardah-RL <wardah@radyalabs.id>
2024-10-02 14:08:52 +0700

    entity event dab migation

---
+++ src/DotnetApiTemplate.Domain/Entities/MsEvent.cs
@@ -0,0 +1,18 @@
+﻿using DotnetApiTemplate.Shared.Abstractions.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetApiTemplate.Domain.Entities
+{
+  public class MsEvent : BaseEntity
+  {
+    public string Name { get; set; } = null!;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Lokasi { get; set; } = null!;
+    public string JumlahTiket { get; set; } = null!;
+  }
+}
